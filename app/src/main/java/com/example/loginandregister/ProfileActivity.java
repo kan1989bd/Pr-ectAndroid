073904ec091c0f110eeecd8b3fde09b0a73b8d6b
Du Nguyễn <nguyenphanduu12@gmail.com>
2020-12-15 09:51:18 +0700

    Load database username

--- app/src/main/java/com/example/loginandregister/ProfileActivity.java
+++ app/src/main/java/com/example/loginandregister/ProfileActivity.java
@@ -1,36 +1,58 @@
 package com.example.loginandregister;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ProfileActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+    private static final String TAG = "ProfileActivity";
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
+
+    TextView txtUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        mAuth = FirebaseAuth.getInstance();
-        onStart();
-    }
+        txtUserName=findViewById(R.id.txt_user_name);
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference=firebaseDatabase.getReference("User").child(firebaseUser.getUid());
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser!=null){
-            Toast.makeText(this,"CC",Toast.LENGTH_SHORT).show();
-        }
-    }
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User user=snapshot.getValue(User.class);
+                assert user != null;
+                txtUserName.setText(user.getUserName());
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
 
     public void BackToMainScreen(View view) {
         Intent intent =new Intent(this,MainScreen.class);
